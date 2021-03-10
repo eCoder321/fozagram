f297d86de30af4e758c73219d501a451a5626591
ecoder321 <femiwonderful@gmail.com>
2021-03-10 09:00:35 -0500

    added a serializer class for comments

--- fozagram/config/routes.rb
+++ fozagram/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :likes, only: [:create]
-  resources :comments, only: [:create, :update]
+  resources :comments, only: [:create, :update, :index]
   resources :images
   resources :users
   post 'users/get_user' => 'users#get_user'
